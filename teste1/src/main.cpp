2119426f777febfa3254bd069b68218486ab8a8a
joaodematejr <joaodematejr@gmail.com>
2019-07-02 23:14:36 -0300

    Teste 1

---
+++ teste1/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup()
+{
+  //BUILTIN_LED = GPIO2 DO ESP8266
+  pinMode(BUILTIN_LED, OUTPUT); //DEFINE O PINO COMO SAÍDA
+}
+
+void loop()
+{
+  digitalWrite(BUILTIN_LED, LOW);  //LED DA PLACA ACENDE (ACIONAMENTO COM SINAL LÓGICO INVERSO PARA O PINO 2)
+  delay(1000);                     //INTERVALO DE 1 SEGUNDO
+  digitalWrite(BUILTIN_LED, HIGH); //LED DA PLACA APAGA (ACIONAMENTO COM SINAL LÓGICO INVERSO PARA O PINO 2)
+  delay(1000);                     //INTERVALO DE 1 SEGUNDO
+}
